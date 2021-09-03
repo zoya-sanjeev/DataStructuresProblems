6508a1bbbc5d95fce1e4f044f8e4b35b6d34f6e2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-03 19:19:53 +0530

    ADDED: program to store prime numbers in a 2d array

---
+++ src/PrimeNumber.java
@@ -0,0 +1,50 @@
+
+public class PrimeNumber {
+	
+	public static boolean primeOrNot(int numberToCheck) {
+		if(numberToCheck==0 || numberToCheck==1) {
+			return false;
+		}
+		else
+		{
+			for(int index=2; index<=numberToCheck/2; index++) {
+				if(numberToCheck%index==0) 
+					return false;
+				
+				
+			}
+			return true;
+		}
+			 
+	      
+	}  
+	
+	public static int[][]  primeNumbers(int start, int end) {
+		
+		int[][] primeNumArray=new int[10][100];
+		
+		for(int i=start;i<=end;i++) {
+			if(primeOrNot(i))
+				primeNumArray[i/100][i%100]=i;
+			
+		}
+		return primeNumArray;
+	}
+	
+	public static void printPrimeNumbers(int[][] primeArray) {
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<100;j++) {
+				if(primeArray[i][j]!=0)
+					System.out.print(primeArray[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int[][] primeArray=primeNumbers(0, 1000);
+		printPrimeNumbers(primeArray);
+
+	}
+
+}
